2e66f5bbe7331299e1f7fd3b1797f5966d0b891a
kenta <kenta.job09@gmail.com>
2020-05-12 11:52:17 +0900

    FirstCommit

---
+++ README.md
@@ -0,0 +1,11 @@
+#Ng-Ukagaka
+## さっくり起動
+※大前提としてNode.jsとNPMとAngularがインストール済とします。
+- 必要な依存環境を揃える  
+  ```
+  npm install
+  ```
+- Angularを起動して動作の確認 
+  ```
+  ng serve
+  ```  
---
+++ src/app/app.config.ts
@@ -0,0 +1,4 @@
+// export const NAR_URL = 'assets/nar/irongirl.zip';
+export const NAR_URL = 'assets/nar/ghoat.zip';
+export const BALLOON_URL = 'https://ikagaka.github.io/cuttlebone/nar/origin.nar';
+export const SHELL_ID = 'master';
